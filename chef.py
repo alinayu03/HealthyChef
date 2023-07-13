47dc056930c64d4287241117092a3df635011777
John Harvardalinayu03 <alinayu@college.harvard.edu>
2023-07-13 18:12:29 -0400

    c

--- chef.py
+++ chef.py
@@ -60,7 +60,7 @@ meal_type = col2.radio(
     "Meal Type", ["Any", "Breakfast", "Lunch", "Dinner", "Snack"])
 
 # LLM setup
-repo_id = "databricks/dolly-v2-3b"
+repo_id = "Writer/camel-5b-hf"
 # model_name = "gpt-3.5-turbo"
 # llm = ChatOpenAI(model_name=model_name, temperature=0.0)
 llm = HuggingFaceHub(
